8ec63be8b95fbe22132d17570bdc40e03b386b93
abiryusuf <yusuf_ctg@yahoo.com>
2020-04-19 20:56:20 -0400

    update

--- server.js
+++ server.js
@@ -10,3 +10,24 @@ const app = express();
 const PORT = process.env.PORT || 8989;
 
 //Sets up initial app to handle data parsing
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Link to css 
+app.use(express.static(path.join(_dirname, "public")));
+
+//Route = Map
+
+//Displays homepage
+app.get("/", (res, req)=>{
+  res.sendFile(path.join(_dirname, "public", "index.html"));
+});
+app.get("/notes", (res, req)=>{
+    res.sendFile(path.join(_dirname, "public", "notes.html"));
+    console.log("Note taker page")
+
+});
+
+
+
